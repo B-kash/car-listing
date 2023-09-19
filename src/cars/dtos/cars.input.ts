fb02d3248663cbae090e10d2cdccd1bc96c96e7c
B-kash <bikashchapagain047@gmail.com>
2023-09-19 23:59:27 +0200

    ✨ add car args and car input

---
+++ src/cars/dtos/cars.input.ts
@@ -0,0 +1,26 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { GearBoxes } from '@prisma/client';
+
+@InputType()
+export class CarInput {
+  @Field({ nullable: true })
+  vin: string;
+
+  @Field({ nullable: true })
+  manufacturer: string; // turn this into a metadata table later
+
+  @Field({ nullable: true })
+  modelDetails: string; // this could be a long string careful when adding it to db
+
+  @Field(() => GearBoxes, { nullable: true })
+  gearBox: GearBoxes; // should be a metadata field as its automatic manual and other
+
+  @Field({ nullable: true })
+  color: string;
+
+  @Field({ nullable: true })
+  mielage: number;
+
+  @Field({ nullable: true })
+  firstRegistrationDate?: Date;
+}
