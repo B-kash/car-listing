8ecb180cfe8ad93ec2c62af8ffff3be9e64c38eb
B-kash <bikashchapagain047@gmail.com>
2023-09-27 13:04:46 +0200

    ✨enable cors

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ export const config = {
   ENV: process.env.ENV || 'production',
   DATABASE_URL: process.env.DATABASE_URL,
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
+  CLIENT_ORIGIN_URL: process.env.CLIENT_ORIGIN_URL || 'http://localhost:3000',
 };
 
 console.log(config);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = config.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: config.CLIENT_ORIGIN_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
   await app.listen(port, async () => {
     console.log(`🚀 Server started at ${await app.getUrl()}`);
   });
